03f44707603b11f8bd897507d0f8ad35f2c31491
jnavno <jnn@suntek.dev>
2025-07-19 12:26:56 -0500

    feat(treeguard): Integrate TreeGuard protobufs, update generation script, and regenerate all protos for official path

--- bin/regen-protos.sh
+++ bin/regen-protos.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-
 set -e
-
 echo "This script requires https://jpa.kapsi.fi/nanopb/download/ version 0.4.9 to be located in the"
 echo "firmware root directory if the following step fails, you should download the correct"
 echo "prebuilt binaries for your computer into nanopb-0.4.9"
 
-# the nanopb tool seems to require that the .options file be in the current directory!
-cd protobufs
-../nanopb-0.4.9/generator-bin/protoc --experimental_allow_proto3_optional "--nanopb_out=-S.cpp -v:../src/mesh/generated/" -I=../protobufs meshtastic/*.proto
+# Go into the protobuf directory
+cd ../protobufs
+
+# Generate the Meshtastic protos
+../nanopb-0.4.9/generator/protoc \
+    --experimental_allow_proto3_optional \
+    "--nanopb_out=-S.cpp -v:../src/mesh/generated/" \
+    -I=. \
+    meshtastic/*.proto
+
+# Go back to the original directory
+cd - > /dev/null
+
+echo "Protobuf generation complete."
